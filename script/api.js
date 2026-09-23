d12f7467abe75371607da8106188575182324e17
Gabriel Rosa <oi5887282@gmailcom>
2026-09-23 19:46:06 -0300

    Adds

--- script/api.js
+++ script/api.js
@@ -4,9 +4,54 @@ const api = {
             const response = await fetch('http://localhost:3000/pensamentos')
             return await response.json()
         } 
-        catch {
+        catch (error) {
             alert('Erro ao buscar pensamentos')
             throw error
+        }
+    },
+
+    async salvarPensamentos(pensamento) {
+        try {
+            const response = await fetch('http://localhost:3000/pensamentos', {
+                method: "POST",
+                headers: {
+                    "Content-type": "application/json"
+                },
+                body: JSON.stringify(pensamento)
+            })
+            return await response.json()
+        } 
+        catch (error) {
+            alert('Erro ao salvar pensamento');  
+            throw error;
+        }
+    },
+
+    async buscarPensamentoPorId(id) {
+        try {
+            const response = await fetch(`http://localhost:3000/pensamentos/${id}`)
+            return await response.json()
+        } 
+        catch (error) {
+            alert('Erro ao buscar pensamento por id'); 
+            throw error;
+        }
+    },
+
+    async editarPensamentos(pensamento) {
+        try {
+            const response = await fetch(`http://localhost:3000/pensamentos/${pensamento.id}`, {
+                method: "PUT",
+                headers: {
+                    "Content-type": "application/json"
+                },
+                body: JSON.stringify(pensamento)
+            })
+            return await response.json()
+        } 
+        catch (error) {
+            alert('Erro ao editar pensamento');  
+            throw error;
         }
     }
 }
--- script/main.js
+++ script/main.js
@@ -1,6 +1,37 @@
 import ui from "./ui.js"
+import api from "./api.js"
 
 //DOMContentLoaded = quando a pagina carregar
 document.addEventListener("DOMContentLoaded", () => {
     ui.renderizarPensamentos()
-}) 
+
+    const formularioPensamento = document.getElementById('pensamento-form')
+    const botaoCancelar = document.getElementById('botao-cancelar')
+
+    formularioPensamento.addEventListener('submit', manipularSubmissaoFormulario)
+    botaoCancelar.addEventListener('click', manipularCancelamento)
+})
+
+async function manipularSubmissaoFormulario(event) {
+    event.preventDefault()
+
+    const id = document.getElementById('pensamento-id').value
+    const conteudo = document.getElementById('pensamento-conteudo').value
+    const autoria = document.getElementById('pensamento-autoria').value
+
+    try {
+        if (id) {
+            await api.editarPensamentos({ id, conteudo, autoria })
+        } else {
+            await api.salvarPensamentos({ conteudo, autoria })
+        }
+        ui.renderizarPensamentos()
+    } 
+    catch {
+        alert('Erro ao salvar pensamento')
+    }
+}
+
+function manipularCancelamento() {
+    ui.limparFormulario();
+}
